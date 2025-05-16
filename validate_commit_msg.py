2300c6e44378a742b801e62a945ba591c9b8907e
Rafael Rocha <santos.rafael@grantoseguros.com>
2025-05-16 11:36:18 -0300

    validar commit

--- validate_commit_msg.py
+++ validate_commit_msg.py
@@ -1,11 +1,15 @@
 import sys
 import re
 
-commit_msg = sys.stdin.read().strip()
+commit_msg_file = sys.argv[1]
 
-# Padrão: tipos do Conventional Commit em lowercase e com uma mensagem
+with open(commit_msg_file, 'r', encoding='utf-8') as f:
+    commit_msg = f.read().strip()
+
+# Padrão: tipo(en-US) ou pt-br seguido de descrição, tudo em minúsculas
 pattern = r'^(feat|fix|docs|style|refactor|test|chore)(\([a-zA-Z0-9]+\))?: .+'
 
 if not re.match(pattern, commit_msg):
-    print("Mensagem de commit inválida. Use o padrão 'tipo(scope)?: descrição' em minúsculas.")
+    print("Mensagem de commit inválida.")
+    print("Use o padrão: 'tipo(scope)?: descrição' em minúsculas, conforme o padrão Conventional Commit.")
     sys.exit(1)
